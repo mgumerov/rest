3e3e89f6e1bbc6cee4352eb6f3a5e73560df0bfc
Maksim Gumerov <maxim.gumerov@massmo.ru>
2016-02-16 20:03:46 +0500

    Maven migration: restructuring, actual pom.xml

--- src/main/java/test/HelloController.java
+++ src/main/java/test/HelloController.java
@@ -6,12 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class HelloController {
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET, produces = "application/json")
         @ResponseBody
 	public Object hello() {
+		new ArrayList<Object>().stream();
 		return new Object() { public final String hello = "hello"; };
 	}
+
 }
